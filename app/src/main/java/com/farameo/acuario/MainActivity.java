c02a29deb35a2925b6e9bc4d59c5c2da550a9370
Farameo <farameo@hotmail.com>
2017-07-03 07:54:54 -0300

    Esquema

--- app/src/main/java/com/farameo/acuario/MainActivity.java
+++ app/src/main/java/com/farameo/acuario/MainActivity.java
@@ -1,8 +1,12 @@
 package com.farameo.acuario;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +16,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -73,6 +81,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_luces) {
+            FGLuces fgLuces = new FGLuces();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.contenedor, fgLuces).commit();
 
         } else if (id == R.id.nav_nivel_agua) {
 
